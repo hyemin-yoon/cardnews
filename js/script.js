c0fa65f5e2012331af6f2c0ccd01a3dce2f784e7
hyemin-yoon <156285516+hyemin-yoon@users.noreply.github.com>
2024-01-17 14:36:35 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    var swiper = new Swiper(".contents", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
